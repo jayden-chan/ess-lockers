830e4c4bc291f3a93cb2220ffbe625e74b8f00fa
Jayden Chan <jaydencn7@gmail.com>
2018-06-11 12:47:17 -0700

    Update scss

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,7 +6,7 @@ class Header extends Component {
       <header>
         <div className="container-fluid">
           <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-            <a className="navbar-brand" href="https://ess.uvic.ca">University of Victoria - Engineering Student Society</a>
+            <a className="navbar-brand" href="http://ess.uvic.ca">University of Victoria - Engineering Student Society</a>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
--- src/components/pages/homepage.js
+++ src/components/pages/homepage.js
@@ -8,16 +8,16 @@ class Homepage extends Component {
           <h1 className="display-4">ESS Locker Registration</h1>
           <h5 className="display-5"><b>Before registering please read the following information:</b></h5>
           <p className="lead" style={{marginTop: 30}}>
-            This is the locker registration page for the lockers in the ELW. The Engineering Students Society controls the registration of lockers in the Engineering Lab Wing (ELW) only. <b>The lockers are registered on a first come first serve basis.</b>
+            This is the locker registration page for the lockers in the ELW. The Engineering Students Society controls the registration of lockers in the Engineering Lab Wing (ELW) only.<b> The lockers are registered on a first come first serve basis.</b>
           </p>
           <p className="lead" style={{marginTop: 30}}>
             If you register a locker, you may keep it until the first week of the next term. After that time, please remove your lock or inform us that you will be continuing to use your locker.
           </p>
           <p className="lead" style={{marginTop: 30}}>
-            These lockers are free, but <b>please</b> don't register for a locker unless you really thing you are going to use it. There are far less lockers than students. You do not need to be in the faculty of Engineering to use a locker.
+            These lockers are free, but <b>please</b> don't register for a locker unless you really think you are going to use it. There are far less lockers than students. You do not need to be in the faculty of Engineering to use a locker.
           </p>
           <p className="lead" style={{marginTop: 30}}>
-            Before you register a locker, you need to <b>physically make sure the one you want is open.</b> Then you <b>have to put a lock on it.</b> If you don't, and the rest of the lockers have been registered, we will delete your registration and give the locker to someone more deserving.
+            Before you register a locker, you need to <b>physically make sure the one you want is open.</b> Then <b>you have to put a lock on it.</b> If you don't, and the rest of the lockers have been registered, we will delete your registration and give the locker to someone more deserving.
           </p>
           <p className="lead">
             <a className="btn btn-primary btn-lg" href="/" role="button">I'm ready to register</a>
