7a7afa37628afc8cbd256e6aeb89599583acfd88
Jayden Chan <jaydencn7@gmail.com>
2018-06-12 12:25:18 -0700

    Fix broken button on homepage

--- src/components/pages/homepage.js
+++ src/components/pages/homepage.js
@@ -19,28 +19,9 @@ class Homepage extends Component {
           <p className="lead" style={{marginTop: 30}}>
             Before you register a locker, you need to <b>physically make sure the one you want is open.</b> Then <b>you have to put a lock on it.</b> If you don't, and the rest of the lockers have been registered, we will delete your registration and give the locker to someone more deserving.
           </p>
-            <button type="button" className="btn btn-primary btn-lg" data-toggle="modal" data-target="#exampleModal">
-              Launch demo modal
-            </button>
-          <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog" role="document">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-                  <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div className="modal-body">
-                  ...
-                </div>
-                <div className="modal-footer">
-                  <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                  <button type="button" className="btn btn-primary">Save changes</button>
-                </div>
-              </div>
-            </div>
-          </div>
+          <a type="button" className="btn btn-primary btn-lg" href="/register">
+            I'm ready to register
+          </a>
         </div>
       </div>
     );
