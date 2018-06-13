4098c23cc0fde8b39bc6c3521f82cef75b4bfbb4
Jayden Chan <jaydencn7@gmail.com>
2018-06-12 23:25:01 -0700

    More work on register form

--- src/components/pages/register.js
+++ src/components/pages/register.js
@@ -5,7 +5,8 @@ class Register extends Component {
     super(props);
     this.state = {
       nameValue: '',
-      emailValue: ''
+      emailValue: '',
+      lockerValue: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -29,18 +30,27 @@ class Register extends Component {
   render() {
     return (
       <div className="container">
-          <form onSubmit={this.handleSubmit} style={{marginTop: 30}}>
-            <div class="form-group">
-              <label for="inputName">Name</label>
-              <input type="text" name="nameValue" placeholder="Enter name" className="form-control" value={this.state.nameValue} onChange={this.handleChange} />
-            </div>
-            <div class="form-group">
-              <label for="inputEmail">Email address</label>
-              <input type="email" name="emailValue" id="inputEmail" placeholder="Enter email" className="form-control" value={this.state.emailValue} onChange={this.handleChange} />
-              <small id="emailHelp" class="form-text text-muted">Your email will not be shared with anyone else. We will send you messages regarding the status of your reservation.</small>
-            </div>
-            <input type="submit" className="btn btn-primary"value="Submit" />
-          </form>
+        <form onSubmit={this.handleSubmit} style={{marginTop: 30}}>
+          <div className="form-group">
+            <label htmlFor="inputName">Name</label>
+            <input type="text" name="nameValue" placeholder="Enter name" className="form-control" value={this.state.nameValue} onChange={this.handleChange} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="inputEmail">Email address</label>
+            <input type="email" name="emailValue" id="inputEmail" placeholder="Enter email" className="form-control" value={this.state.emailValue} onChange={this.handleChange} />
+            <small id="emailHelp" className="form-text text-muted">Your email will not be shared with anyone else. We will send you messages regarding the status of your reservation.</small>
+          </div>
+          <div className="form-group" style={{maxWidth: '40%'}}>
+            <label htmlFor="lockerDrop">Choose your locker</label>
+            <select id="lockerDrop" className="form-control" name="lockerValue" value={this.state.lockerValue} onChange={this.handleChange}>
+              <option value="coconut">1</option>
+              <option value="grapefruit">2</option>
+              <option value="lime">3</option>
+              <option value="mango">4</option>
+            </select>
+          </div>
+          <input type="submit" className="btn btn-primary"value="Submit" />
+        </form>
       </div>
     );
   }
