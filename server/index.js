abc2a495a325863badadc582a49b7f039a2a3608
Jayden Chan <jaydencn7@gmail.com>
2018-09-12 20:09:56 -0700

    Make lockers table a variable

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3001;
 const RESET_TIMEOUT = process.env.LOCKER_RESET_TIME || 10000;
 const SQL_USER = process.env.LOCKER_SQL_USER || 'lockers';
 const SQL_PASSWORD = process.env.LOCKER_SQL_PASSWORD || 'no_password';
+const SQL_TABLE = process.env.LOCKER_SQL_TABLE || 'lockers';
 
 app.use((req, res, next) => {
   global.connection = mysql.createPool({
@@ -32,7 +33,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/api/available', (req, res) => {
-  const query = sqlstring.format('SELECT number FROM lockers WHERE status = ?', 'open');
+  const query = sqlstring.format('SELECT number FROM ?? WHERE status = ?', SQL_TABLE, 'open');
   connection.query(query, (error, results, fields) => {
     if (error) {
       res.status(500).send('Database query failed.');
@@ -45,11 +46,11 @@ app.get('/api/available', (req, res) => {
 app.post('/api/new', (req, res) => {
   if (req.body.name !== '' && req.body.email !== '' && req.body.locker !== '') {
 
-    const query1 = sqlstring.format('SELECT * FROM lockers WHERE email = ?', req.body.email);
+    const query1 = sqlstring.format('SELECT * FROM ?? WHERE email = ?', SQL_TABLE, req.body.email);
     connection.query(query1, (error, results, fields) => {
       if(results.length === 0) {
-        const query2 = sqlstring.format('UPDATE lockers SET name = ?, email = ?, submitted = NOW(), status = ? WHERE number = ?',
-          [req.body.name, req.body.email, 'closed', req.body.locker]);
+        const query2 = sqlstring.format('UPDATE ?? SET name = ?, email = ?, submitted = NOW(), status = ? WHERE number = ?',
+          [SQL_TABLE, req.body.name, req.body.email, 'closed', req.body.locker]);
 
         connection.query(query2, (error, results, fields) => {
           if (error) {
@@ -83,8 +84,8 @@ app.post('/api/new', (req, res) => {
 
 app.post('/api/renew', (req, res) => {
   if (req.body.email !== '') {
-    const query = sqlstring.format('UPDATE lockers SET status = ? WHERE status = ? AND email = ?',
-      ['closed', 'pending', req.body.email]);
+    const query = sqlstring.format('UPDATE ?? SET status = ? WHERE status = ? AND email = ?',
+      [SQL_TABLE, 'closed', 'pending', req.body.email]);
 
     connection.query(query, (error, results, fields) => {
       if (error) {
@@ -113,14 +114,14 @@ app.post('/api/renew', (req, res) => {
 
 app.post('/api/deregister/code', (req, res) => {
   if (req.body.number !== '' && req.body.email !== '') {
-    const query = sqlstring.format('SELECT email FROM lockers WHERE number = ?', req.body.number);
+    const query = sqlstring.format('SELECT email FROM ?? WHERE number = ?', SQL_TABLE, req.body.number);
     connection.query(query, (error, results, fields) => {
       if (error) {
         res.status(500).send('Database query failed.');
       } else if (results.length === 1 && results[0].email === req.body.email) {
         const resetCode = Math.floor(100000 + Math.random() * 900000);
-        const query = sqlstring.format('UPDATE lockers SET reset_code = ? WHERE number = ?',
-          [resetCode, req.body.number]);
+        const query = sqlstring.format('UPDATE ?? SET reset_code = ? WHERE number = ?',
+          [SQL_TABLE, resetCode, req.body.number]);
 
         connection.query(query, (error, results, fields) => {
           if (error) {
@@ -143,7 +144,7 @@ app.post('/api/deregister/code', (req, res) => {
         });
 
         var timer = setTimeout(() => {
-          const query = sqlstring.format('UPDATE lockers SET reset_code = NULL');
+          const query = sqlstring.format('UPDATE ?? SET reset_code = NULL', SQL_TABLE);
           connection.query(query, (error, results, fields) => {
             if (error) {
               console.log('Reset codes not reset');
@@ -161,15 +162,15 @@ app.post('/api/deregister/code', (req, res) => {
 
 app.delete('/api/deregister/confirm', (req, res) => {
   if (req.body.code !== '') {
-    const query1 = sqlstring.format('SELECT * FROM lockers WHERE reset_code = ?', req.body.code)
+    const query1 = sqlstring.format('SELECT * FROM ?? WHERE reset_code = ?', SQL_TABLE, req.body.code)
     connection.query(query, (error, results, fields) => {
       if (error) {
         res.status(500).send('Database query failed');
       } else if (results.length !== 1) {
         res.status(400).send('Invalid reset code. Please try again later');
       } else {
-        const query2 = sqlstring.format('UPDATE lockers SET reset_code = ?, status = ? WHERE reset_code = ?',
-          [null, 'open', req.body.code]);
+        const query2 = sqlstring.format('UPDATE ?? SET reset_code = ?, status = ? WHERE reset_code = ?',
+          [SQL_TABLE, null, 'open', req.body.code]);
 
         connection.query(query, (error, results, fields) => {
           if (error) {
