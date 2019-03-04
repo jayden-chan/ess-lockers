6d27d26d89698ad066b95d89cf9c8d9356533e95
Jayden Chan <jaydencn7@gmail.com>
2019-03-03 18:51:07 -0800

    Fix admin file

--- admin/src/components/pages/admin.js
+++ admin/src/components/pages/admin.js
@@ -23,10 +23,10 @@ class Admin extends Component {
       headers: {
         'Authorization': API_KEY,
         'Content-Type': 'application/json',
-      };
+      },
     })
       .then(res => {
-        if(res.status === 200) {
+        if (res.status === 200) {
           return res.json();
         } else {
           console.log('Lockers API req failed (Code: '+ res.status + ')');
@@ -34,7 +34,7 @@ class Admin extends Component {
         }
       })
       .then(json => {
-        this.setState({all: json})
+        this.setState({all: json});
       });
   }
 
@@ -43,7 +43,7 @@ class Admin extends Component {
   }
 
   assembleTable(data) {
-    if(data === undefined) {
+    if (data === undefined) {
       return (
         <table className="table">
           <thead>
@@ -57,7 +57,7 @@ class Admin extends Component {
             </tr>
           </tbody>
         </table>
-      )
+      );
 
     }
     return (
@@ -74,15 +74,23 @@ class Admin extends Component {
         <tbody>
           {data.map(entry => {
             // Filter the locker table based on the contents of the search bar
-            const containsName = entry.name.toLowerCase().includes(this.state.searchNameString.toLowerCase());
-            const containsStatus = entry.status.toLowerCase().includes(this.state.searchNameString.toLowerCase());
+            const containsName = entry.name
+              .toLowerCase()
+              .includes(this.state.searchNameString.toLowerCase());
+
+            const containsStatus = entry.status
+              .toLowerCase()
+              .includes(this.state.searchNameString.toLowerCase());
+
             let containsEmail = false;
 
-            if(entry.email !== null) {
-              containsEmail = entry.email.toLowerCase().includes(this.state.searchNameString.toLowerCase())
+            if (entry.email !== null) {
+              containsEmail = entry.email
+                .toLowerCase()
+                .includes(this.state.searchNameString.toLowerCase());
             }
 
-            if(containsName || containsEmail || containsStatus) {
+            if (containsName || containsEmail || containsStatus) {
               return (
                 <tr key={entry.number}>
                   <th scope="row">{entry.number}</th>
@@ -91,14 +99,14 @@ class Admin extends Component {
                   <td>{entry.submitted}</td>
                   <td>{entry.status}</td>
                 </tr>
-              )
+              );
             } else {
               return (<div></div>);
             }
           })}
         </tbody>
       </table>
-    )
+    );
   }
 
   handleChange(event) {
@@ -107,7 +115,7 @@ class Admin extends Component {
     const name = target.name;
 
     this.setState({
-      [name]: value
+      [name]: value,
     });
   }
 
@@ -118,8 +126,9 @@ class Admin extends Component {
           <div className='jumbotron'>
             <h4 className='display-4'>Administration</h4>
             <p>
-              This page is intended to serve as a basic interface for viewing the status of the lockers DB.
-              If you need to perform more advanced actions, please contact the lockers maintainer.
+              This page is intended to serve as a basic interface for viewing the status of
+              the lockers DB. If you need to perform more advanced actions, please contact
+              the lockers maintainer.
             </p>
           </div>
           <h5 className='display-5'>All Lockers</h5>
@@ -128,9 +137,9 @@ class Admin extends Component {
             <div className='form-group'>
               <div className='form-row'>
                 <div className='form-group col-md-4'>
-                  <input type='text' name='searchNameString' id='searchNameString' 
-                    placeholder='Search by name, email, or status' className='form-control' 
-                    value={this.state.searchNameString} onChange={this.handleChange} 
+                  <input type='text' name='searchNameString' id='searchNameString'
+                    placeholder='Search by name, email, or status' className='form-control'
+                    value={this.state.searchNameString} onChange={this.handleChange}
                   />
                 </div>
               </div>
