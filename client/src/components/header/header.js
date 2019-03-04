9a61339858f97ad1667b7adce72f0d6f95f385cd
Jayden Chan <jaydencn7@gmail.com>
2019-03-03 19:26:51 -0800

    Fix header width on mobile

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,14 +1,44 @@
+/* global window */
+
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 0,
+    };
+
+    this.updateWindowSize = this.updateWindowSize.bind(this);
+  }
+
+  updateWindowSize() {
+    this.setState({ width: window.innerWidth });
+  }
+
+  componentDidMount() {
+    this.updateWindowSize();
+    window.addEventListener('resize', this.updateWindowSize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowSize);
+  }
+
+  getHeaderText() {
+    return (this.state.width < 600)
+      ? 'UVic - ESS'
+      : 'University of Victoria - Engineering Student Society';
+  }
+
   render() {
     return (
       <header>
         <div className="container">
           <nav className="navbar navbar-expand-md navbar-dark bg-primary">
             <a className="navbar-brand" href="http://ess.uvic.ca">
-              University of Victoria - Engineering Student Society
+              {this.getHeaderText()}
             </a>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav ml-auto">
